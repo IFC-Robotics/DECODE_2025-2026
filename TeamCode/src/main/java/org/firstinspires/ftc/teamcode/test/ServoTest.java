d4540551e9756aae91388bfa08c8a0993959d88e
britishsailor <ttlim830@gmail.com>
2025-10-14 15:44:26 -0700

    Add ServoTest.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/test/ServoTest.java
@@ -0,0 +1,18 @@
+package org.firstinspires.ftc.teamcode.test;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import org.firstinspires.ftc.teamcode.robot.ServoClass;
+
+@TeleOp(name = "ServoTest")
+public class ServoTest extends LinearOpMode {
+    public void runOpMode() {
+        ServoClass servoLauncher = new ServoClass("servo_launcher", "down", 0.0, "middle", 0.5, "up", 1.0, 0.5, 500, false);
+        servoLauncher.init(this);
+        waitForStart();
+
+        while (opModeIsActive()) {
+            servoLauncher.teleOpAssistMode(gamepad1.a, gamepad1.b, gamepad1.y);
+        }
+    }
+}
